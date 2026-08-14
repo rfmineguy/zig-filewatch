53325516adf0ecfb711e62d8b5c06f7b6faf55e6
Riley Fischer <rfmineguy@gmail.com>
2026-08-13 21:45:57 -0700

    fix windows build failure in test executable - argument iteration requires an allocator to work

--- src/test_main.zig
+++ src/test_main.zig
@@ -2,9 +2,11 @@ const std = @import("std");
 
 pub fn main(init: std.process.Init) !u8 {
     std.debug.print("Hello from test_main\n", .{});
-    var it = init.minimal.args.iterate();
+    var it = try init.minimal.args.iterateAllocator(init.gpa);
     std.debug.print("args: [", .{});
     while (it.next()) |arg| std.debug.print("{s}, ", .{arg});
     std.debug.print("]\n", .{});
+
+    try std.Io.sleep(init.io, std.Io.Duration.fromMilliseconds(4), .awake);
     return 1;
 }
