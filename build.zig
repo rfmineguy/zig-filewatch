fdd9cb4108491d0f22f6525171944dba57dcf882
Riley Fischer <rfmineguy@gmail.com>
2026-08-17 18:46:43 -0700

    link libc

--- build.zig
+++ build.zig
@@ -15,6 +15,7 @@ pub fn build(b: *std.Build) void {
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
+            .link_libc = true,
             .imports = &.{
                 .{ .name = "zig_filewatch", .module = mod },
             },
